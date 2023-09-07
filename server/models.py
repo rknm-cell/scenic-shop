40beceb8800f1bf97a79949e1feae51fb3b5fc2b
rknm-cell <racknahm@gmail.com>
2023-09-07 07:39:33 -0400

    initializing models

--- server/models.py
+++ server/models.py
@@ -3,4 +3,34 @@ from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
-# Models go here!
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String)
+
+class Department(db.Model):
+    __tablename__ = "departments"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=false)
+
+class Project(db.Model):
+    __tablename__ = "projects"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    client_id = db.Column(db.Integer, db.ForeignKey('clients.id'), nullable=False)
+
+class ProjectItems(db.Model):
+    __tablename__ = "projectitems"
+    id = db.Column(db.Integer)
+    name = db.Column(db.String)
+    description = db.Column(db.String)
+    project_id = db.Column(db.Integer, db.ForeignKey('projects.id'), nullable=False)
+
+
+class Client(db.Model):
+    __tablename__ = "clients"
+    projects = db.relationshiop('Projects', backref='clients')
+
+class ClientProjects(db.Model):
+    __tablename__ = "clientprojects"    
