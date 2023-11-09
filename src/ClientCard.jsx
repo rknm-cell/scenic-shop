d1a507a9d7163479bab84155fb899c67a2595ff3
rknm-cell <racknahm@gmail.com>
2023-11-08 22:41:02 -0500

    added array render for client card

--- src/ClientCard.jsx
+++ src/ClientCard.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import ProjectCard from './ProjectCard'
+import ProjectContainer from './ProjectContainer'
 
-const ClientCard = () => {
+const ClientCard = ({projects}) => {
+
+
+  function handleClientProjects(){
+    return (
+      projects.map((project) => {
+        <ProjectContainer project={project}/>
+      })
+    )
+  }
   return (
-    <div>ClientCard</div>
+    <><h1>{client.name}</h1>
+    <p>{client.description} </p>
+    
+    </>
   )
 }
 
--- src/DepartmentCard.jsx
+++ src/DepartmentCard.jsx
@@ -4,10 +4,12 @@ const DepartmentCard = (department) => {
 
   
     
-
-  }
   return (
-    <></>
+    <>
+    <h1>{department.name}</h1>
+    <p>{department.description}</p>
+    
+    </>
   )
 }
 
